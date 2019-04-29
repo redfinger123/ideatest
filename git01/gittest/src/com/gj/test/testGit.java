1d1096d6de2a781740a63890f453887f0745bc60
gj <1714683258@qq.com>
2019-04-29 20:41:48 +0800

    提交到远程仓库

---
+++ git01/gittest/src/com/gj/test/testGit.java
@@ -0,0 +1,12 @@
+package com.gj.test;
+
+public class testGit {
+    public static void main(String[] args) {
+        System.out.println("输出git");
+        System.out.println("输出git");
+        System.out.println("输出git");
+        System.out.println("输出git");
+        System.out.println("输出git");
+    }
+
+}
